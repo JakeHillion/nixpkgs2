3083eb010bdfb84e8c5c12075f2d79804efe6033
Jake Hillion <jake@hillion.co.uk>
2024-12-22 19:38:24 +0000

    nixos/containers: add copyResolvConf option

--- nixos/modules/virtualisation/nixos-containers.nix
+++ nixos/modules/virtualisation/nixos-containers.nix
@@ -107,7 +107,9 @@ let
         "/nix/var/nix/profiles/per-container/$INSTANCE" \
         "/nix/var/nix/gcroots/per-container/$INSTANCE"
 
-      cp --remove-destination /etc/resolv.conf "$root/etc/resolv.conf"
+      if [ "$COPY_RESOLV_CONF" = 1 ]; then
+        cp --remove-destination /etc/resolv.conf "$root/etc/resolv.conf"
+      fi
 
       declare -a extraFlags
 
@@ -633,6 +635,14 @@ in
               '';
             };
 
+            copyResolvConf = mkOption {
+              type = types.bool;
+              default = true;
+              description = ''
+                Whether to copy host /etc/resolv.conf into the container.
+              '';
+            };
+
             privateNetwork = mkOption {
               type = types.bool;
               default = false;
@@ -938,6 +948,7 @@ in
         { text =
             ''
               SYSTEM_PATH=${cfg.path}
+              COPY_RESOLV_CONF=${if cfg.copyResolvConf then "1" else "0"}
               ${optionalString cfg.privateNetwork ''
                 PRIVATE_NETWORK=1
                 ${optionalString (cfg.hostBridge != null) ''
